b6fa75cb8a18e21ddd36672731ed16c5dcd01c0d
Raja Jawahar <rajajawahar77@gmail.com>
2018-04-06 19:43:21 +0530

    Title Modified in Sample

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Welcome to Flutter',
+       home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text('Hello World'),
+        ),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
